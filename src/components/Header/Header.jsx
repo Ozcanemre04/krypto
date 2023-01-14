ca1284281c8ec80254f36850de9c83869940dfbd
ozcanemre <ozcan.emre1608@gmail.com>
2023-01-14 17:07:21 +0100

    header responsive and top ellipsde added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import './Header.scss'
+import line1 from '../../assets/Line1.png'
+import line2 from '../../assets/Line2.png'
+import line3 from '../../assets/Line3.png'
 const Header = () => {
   return (
     <header>
         <div className='header-container'>
         <h1>KRYPTO</h1>
-        <span>x</span>
+        <div className='lines'>
+          <img src={line1} alt="" />
+          <img src={line2} alt="" />
+          <img src={line3} alt="" />
+        </div>
+        <nav>
+          <ul>
+            <li>about</li>
+            <li>pricing</li>
+            <li>contact</li>
+            <li><span></span>buy nfts</li>
+          </ul>
+        </nav>
         </div>
 
     </header>
