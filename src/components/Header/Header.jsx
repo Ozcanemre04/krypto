95638fd546c49b4c8f68c43bdf202ec8d75d57a8
ozcanemre <ozcan.emre1608@gmail.com>
2023-01-16 19:25:40 +0100

    responsive-menu and end

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.scss'
 import line1 from '../../assets/Line1.png'
 import line2 from '../../assets/Line2.png'
 import line3 from '../../assets/Line3.png'
 const Header = () => {
+  const [open,setOpen]= useState(false)
   return (
     <header>
         <div className='header-container'>
         <h1>KRYPTO</h1>
-        <div className='lines'>
+        <div className={open? 'active-lines' : 'lines'} onClick={()=>{setOpen(current=>!current)}}>
           <img src={line1} alt="" />
           <img src={line2} alt="" />
           <img src={line3} alt="" />
@@ -22,7 +23,16 @@ const Header = () => {
           </ul>
         </nav>
         </div>
-
+        
+         <nav className={open? 'active-menu' : 'menu'}>
+           <ul>
+            <li className={open? 'active-list' : 'list'}>about</li>
+            <li className={open? 'active-list' : 'list'}>pricing</li>
+            <li className={open? 'active-list' : 'list'}>contact</li>
+            <li className={open? 'active-list' : 'list'}>buy nfts</li>
+           </ul>
+         </nav>  
+      
     </header>
   )
 }
